9c6d26701ea69f966bfb5e871ea373a6acd8d512
Ray Kwan <kwanlokto@gmail.com>
2019-01-19 19:54:45 -0500

    implemented CNN algo

--- image_capture.py
+++ image_capture.py
@@ -2,42 +2,26 @@
 import k_NN
 import checkModel
 import readCSV
-import cv2, time
+import cv2
+import kaggle_CNN
 import numpy as np
-# from sklearn.neighbors import KNeighborsRegressor
 
 alphabet = {0: 'a', 1: 'b', 2:'c', 3: 'd', 4: 'e',
             5: 'f', 6: 'g', 7: 'h', 8: 'i', 9: 'j',
             10: 'k', 11: 'l', 12: 'm', 13: 'n', 14: 'o',
             15: 'p', 16: 'q', 17: 'r', 18: 's', 19: 't',
-            20:'u', 21: 'v', 22: 'w', 23: 'x', 24: 'y', 25: 'z'}
+            20: 'u', 21: 'v', 22: 'w', 23: 'x', 24: 'y', 25: 'z'}
 
 trainingSet = readCSV.readCSV('./data_set/train.csv')
 trainingPoints = [i[1:] for i in trainingSet]
 trainingLabels = [i[0] for i in trainingSet]
-
-def runModel(testPoint):
-    k = 60
-    # neigh = KNeighborsRegressor(n_neighbors=k)
-    #
-    # neigh.fit(trainingPoints, trainingLabels)
-    # return neigh.predict([testPoint])
-    neighbours = k_NN.getNearestNeighbours(trainingSet, testPoint, k)
-    result = k_NN.getResponse(neighbours)
-    return result
-
+model = kaggle_CNN.load_model()
 
 cap_region_x_begin = 0.5  # start point/total width
 cap_region_y_end = 0.8  # start point/total width
-#1. Create an object. Zero for external camera
-video=cv2.VideoCapture(0)
+video = cv2.VideoCapture(0)
 
-#8. a variable
-previous = None
-a = 0
 while True:
-    a = a + 1
-
     #3. Create a frame object
     check, frame = video.read()
 
@@ -49,30 +33,23 @@ while True:
     cv2.rectangle(gray, (gray.shape[1] - 560, 0),
                   (gray.shape[1], 560), (255, 0, 0), 2) #Display the rectangle on the right
 
-
     #4. Show the frame!
     cv2.imshow("Capturing", gray)
-
-    #5. For press any key to out (miliseconds)
-    # cv2.waitKey(0)
 
     #7. For playing
     key=cv2.waitKey(1)
 
     #9. For getting only info from square region
-
     img = gray[0:560, gray.shape[1] - 560:gray.shape[1]]
     cv2.imshow("hand", img)
     small = cv2.resize(img, (0, 0), fx=0.05, fy=0.05)  # resize image to 240 x 240 px
     hand_pixels = small.flatten()
-
     if key == ord('q'):
         break
     elif key == ord('a'):
-        label = runModel(hand_pixels)
-        # if previous == label:
+        label = kaggle_CNN.run_model(model, np.array([hand_pixels]))
+        # label = run_model(hand_pixels)
         print(alphabet[label])
-        # previous = label
 
 #2. Shutdown the camera
 video.release()
